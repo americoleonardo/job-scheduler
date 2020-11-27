ff27ea92ef6b6f0e3caebe095482f49e287a45c6
Leonardo <leonardoakeisman@gmail.com>
2020-11-27 15:27:13 -0300

    feat: created scheduler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
-  imports: []
+  imports: [SchedulerModule]
 })
 export class AppModule {}
