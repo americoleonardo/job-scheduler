23a9adb78a2cd3b14c2118d0337cfc9141182df3
Leonardo <leonardoakeisman@gmail.com>
2020-11-28 12:44:51 -0300

    feat: added scheduler.moudule.ts

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SchedulerController } from './scheduler.controller';
+import { SchedulerService } from './scheduler.service';
 
-@Module({})
+@Module({
+  controllers: [SchedulerController],
+  providers: [SchedulerService]
+})
 export class SchedulerModule {}
