16b6d1d84d9fd63f8e37c9558dc087aa3f3342a9
Leonardo <leonardoakeisman@gmail.com>
2020-11-30 19:17:01 -0300

    feat: typed scheduler interface params

--- src/scheduler/interfaces/services/scheduler.interface.ts
+++ src/scheduler/interfaces/services/scheduler.interface.ts
@@ -1,9 +1,7 @@
 import { Scheduler } from "../../models/scheduler.model";
 
 export interface IScheduler {
-    getAll(): Scheduler[];
-
     schedule(description: string, finishDate: any, timeEstimated: number): Scheduler;
 
-    addToQueue(scheduler: Scheduler): boolean;
+    addNode(scheduler: Scheduler): boolean;
 }
