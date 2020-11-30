0c58d43d72abbece93043c25d1a0c16fac5eb788
Leonardo <leonardoakeisman@gmail.com>
2020-11-30 19:17:14 -0300

    feat: started business logic

--- src/scheduler/services/scheduler.service.ts
+++ src/scheduler/services/scheduler.service.ts
@@ -5,10 +5,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class SchedulerService implements IScheduler {
-    private queueScheduler: Scheduler[] = [];
+    private readonly MAX_INTERVAL: number = 8;
 
-    getAll(): Scheduler[] {
-      return this.queueScheduler;
+    private queueScheduler: Array<object> = [];
+
+    getAll(): Array<object> {
+      return new Array<object>();
     }
 
     schedule(description: string, finishDate: any, timeEstimated: number): Scheduler {
@@ -19,14 +21,36 @@ export class SchedulerService implements IScheduler {
         timeEstimated
       };
 
-      this.addToQueue(scheduler);
+      this.addNode(scheduler);
 
       return scheduler;
     }
 
-    addToQueue(item: Scheduler): boolean {
-      console.log("add to queue method");
-      console.log(item);
+    addNode(item: Scheduler): boolean {
+      if (!this.queueScheduler.length) {
+        this.queueScheduler.push(new Array(item));
+
+        return true;
+      }
+
+      this.queueScheduler.map((jobs, idx) => {
+        let amountTime: number = 0;
+
+        for (let i in jobs) {
+          amountTime = amountTime + Number(jobs[i].timeEstimated);
+        }
+
+        amountTime = amountTime + Number(item.timeEstimated);
+
+        if (amountTime <= this.MAX_INTERVAL) {
+            Object(jobs).push(item);
+
+            this.queueScheduler[idx] = jobs;
+        } else {
+            this.queueScheduler.push(new Array(item));
+        }
+      }, item);
+      console.log(this.queueScheduler);
 
       return true;
     }
