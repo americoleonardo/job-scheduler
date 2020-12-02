a690629c6b29fcd1443ab0c8799742c93dfc69ad
Leonardo <leonardoakeisman@gmail.com>
2020-12-02 20:07:24 -0300

    feat: created scheduler service unity test

--- src/scheduler/services/scheduler.service.spec.ts
+++ src/scheduler/services/scheduler.service.spec.ts
@@ -1,67 +1,43 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { IScheduler } from "../interfaces/services/scheduler.interface";
-import { Scheduler } from '../models/scheduler.model';
-import { v4 as uuidv4 } from 'uuid';
-import { CreateSchedulerDTO } from "../dto/CreateSchedulerDTO";
+import { Test } from '@nestjs/testing';
+import { SchedulerService } from "./scheduler.service";
+import {CreateSchedulerDTO} from "../dto/CreateSchedulerDTO";
+import {Scheduler} from "../models/scheduler.model";
 
-@Injectable()
-export class SchedulerService implements IScheduler {
-    private readonly logger = new Logger(SchedulerService.name);
+describe('SchedulerService', () => {
+  let schedulerService;
 
-    private readonly MAX_INTERVAL: number = 8;
+  beforeEach(async() => {
+    const module = await Test.createTestingModule({
+      providers: [
+        SchedulerService
+      ]
+    }).compile();
 
-    private queueScheduler: Array<object> = [];
+    schedulerService = await module.get<SchedulerService>(SchedulerService);
+  });
 
-    getAll(): Array<object> {
-      return this.queueScheduler;
-    }
+  it('Const MAX_INTERVAL must be scheduled to each 8 hours', () => {
+      expect(schedulerService.MAX_INTERVAL).toEqual(8);
+  });
 
-    schedule(schedulerDTO: CreateSchedulerDTO): Scheduler {
-      const { description, finishDate, timeEstimated } = schedulerDTO;
+  describe ('getAll', () => {
+    it('List empty scheduler queue when application start.', () => {
+      expect(schedulerService.getAll()).toHaveLength(0);
+    });
+  });
 
-      const scheduler: Scheduler = {
-        id: uuidv4(),
-        description,
-        finishDate,
-        timeEstimated
+  describe ('schedule', () => {
+    it('Add item to queue and verify if was inserted.', () => {
+      const schedulerDTO: CreateSchedulerDTO = {
+        description: "description",
+        finishDate: "2020-01-01 00:00:00",
+        timeEstimated: 3
       };
 
-      this.addNode(scheduler);
+      let result = schedulerService.schedule(schedulerDTO)
+      let obj: any = schedulerService.getAll();
 
-      return scheduler;
-    }
-
-    addNode(item: Scheduler): boolean {
-      this.logger.log(">> Trying to add job");
-      if (!this.queueScheduler.length) {
-        this.queueScheduler.push(new Array(item));
-
-        this.logger.log(">> Queue without jobs. Added succesfully.");
-        return true;
-      }
-
-      let hasInserted: boolean;
-      this.queueScheduler.map((jobs, idx) => {
-        let amountTime: number = Object(jobs).reduce((total, job) => {
-          return total + Number(job.timeEstimated)
-        }, 0);
-
-        amountTime = amountTime + Number(item.timeEstimated);
-
-        if (amountTime <= this.MAX_INTERVAL && !hasInserted) {
-          Object(jobs).push(item);
-
-          this.queueScheduler[idx] = jobs;
-          hasInserted = true;
-        } else {
-            if (!hasInserted) {
-              this.queueScheduler.push(new Array(item));
-              hasInserted = true;
-            }
-        }
-      }, item);
-
-      this.logger.log(">> Job added succesfully.");
-      return true;
-    }
-}
+      expect(Object(obj[0][0])).toEqual(result);
+    });
+  });
+});
