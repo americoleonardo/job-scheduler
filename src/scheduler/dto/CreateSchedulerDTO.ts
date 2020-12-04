1461034542a9056eeb8afbf913d31a461e367b61
Leonardo <leonardoakeisman@gmail.com>
2020-12-03 22:35:18 -0300

    feat: installed swagger

--- src/scheduler/dto/CreateSchedulerDTO.ts
+++ src/scheduler/dto/CreateSchedulerDTO.ts
@@ -1,12 +1,27 @@
 import { IsNotEmpty } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSchedulerDTO {
+    @ApiProperty({
+      description: 'Description of scheduler',
+      minimum: 1,
+      default: 'Scheduler example'
+    })
     @IsNotEmpty()
     public description: string;
 
+    @ApiProperty({
+      description: 'Timestamp of scheduler',
+      default: '2020-11-11 08:00:00'
+    })
     @IsNotEmpty()
     public finishDate: any;
 
+    @ApiProperty({
+      description: 'Time estimated to finish schedule job',
+      minimum: 1,
+      default: 1
+    })
     @IsNotEmpty()
     public timeEstimated: number;
 }
